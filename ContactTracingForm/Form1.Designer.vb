13855a8ae4aec1cd740234c2c5be9ffbc9fb292e
jbelardo27 <jbbimol27@gmail.com>
2022-06-07 03:00:46 +0800

    added questions

--- ContactTracingForm/Form1.Designer.vb
+++ ContactTracingForm/Form1.Designer.vb
@@ -22,10 +22,253 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.lnbox = New System.Windows.Forms.TextBox()
+        Me.fnbox = New System.Windows.Forms.TextBox()
+        Me.mibox = New System.Windows.Forms.TextBox()
+        Me.addressbox = New System.Windows.Forms.TextBox()
+        Me.cnbox = New System.Windows.Forms.TextBox()
+        Me.agebox = New System.Windows.Forms.TextBox()
+        Me.emailbox = New System.Windows.Forms.TextBox()
+        Me.lnlabel = New System.Windows.Forms.Label()
+        Me.fnlabel = New System.Windows.Forms.Label()
+        Me.milabel = New System.Windows.Forms.Label()
+        Me.addresslabel = New System.Windows.Forms.Label()
+        Me.agelabel = New System.Windows.Forms.Label()
+        Me.cnlabel = New System.Windows.Forms.Label()
+        Me.emaillabel = New System.Windows.Forms.Label()
+        Me.question1 = New System.Windows.Forms.Label()
+        Me.question2 = New System.Windows.Forms.Label()
+        Me.question3 = New System.Windows.Forms.Label()
+        Me.question4 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lnbox
+        '
+        Me.lnbox.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lnbox.Location = New System.Drawing.Point(12, 12)
+        Me.lnbox.Name = "lnbox"
+        Me.lnbox.Size = New System.Drawing.Size(400, 29)
+        Me.lnbox.TabIndex = 0
+        '
+        'fnbox
+        '
+        Me.fnbox.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.fnbox.Location = New System.Drawing.Point(418, 12)
+        Me.fnbox.Name = "fnbox"
+        Me.fnbox.Size = New System.Drawing.Size(400, 29)
+        Me.fnbox.TabIndex = 1
+        '
+        'mibox
+        '
+        Me.mibox.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.mibox.Location = New System.Drawing.Point(824, 12)
+        Me.mibox.Name = "mibox"
+        Me.mibox.Size = New System.Drawing.Size(29, 29)
+        Me.mibox.TabIndex = 2
+        '
+        'addressbox
+        '
+        Me.addressbox.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.addressbox.Location = New System.Drawing.Point(12, 68)
+        Me.addressbox.Name = "addressbox"
+        Me.addressbox.Size = New System.Drawing.Size(789, 29)
+        Me.addressbox.TabIndex = 3
+        '
+        'cnbox
+        '
+        Me.cnbox.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.cnbox.Location = New System.Drawing.Point(12, 124)
+        Me.cnbox.Name = "cnbox"
+        Me.cnbox.Size = New System.Drawing.Size(279, 29)
+        Me.cnbox.TabIndex = 4
+        '
+        'agebox
+        '
+        Me.agebox.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.agebox.Location = New System.Drawing.Point(807, 68)
+        Me.agebox.Name = "agebox"
+        Me.agebox.Size = New System.Drawing.Size(46, 29)
+        Me.agebox.TabIndex = 5
+        '
+        'emailbox
+        '
+        Me.emailbox.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.emailbox.Location = New System.Drawing.Point(297, 124)
+        Me.emailbox.Name = "emailbox"
+        Me.emailbox.Size = New System.Drawing.Size(504, 29)
+        Me.emailbox.TabIndex = 6
+        '
+        'lnlabel
+        '
+        Me.lnlabel.AutoSize = True
+        Me.lnlabel.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lnlabel.Location = New System.Drawing.Point(12, 44)
+        Me.lnlabel.Name = "lnlabel"
+        Me.lnlabel.Size = New System.Drawing.Size(84, 21)
+        Me.lnlabel.TabIndex = 7
+        Me.lnlabel.Text = "Last Name"
+        '
+        'fnlabel
+        '
+        Me.fnlabel.AutoSize = True
+        Me.fnlabel.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.fnlabel.Location = New System.Drawing.Point(418, 44)
+        Me.fnlabel.Name = "fnlabel"
+        Me.fnlabel.Size = New System.Drawing.Size(86, 21)
+        Me.fnlabel.TabIndex = 8
+        Me.fnlabel.Text = "First Name"
+        '
+        'milabel
+        '
+        Me.milabel.AutoSize = True
+        Me.milabel.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.milabel.Location = New System.Drawing.Point(824, 44)
+        Me.milabel.Name = "milabel"
+        Me.milabel.Size = New System.Drawing.Size(28, 21)
+        Me.milabel.TabIndex = 9
+        Me.milabel.Text = "MI"
+        '
+        'addresslabel
+        '
+        Me.addresslabel.AutoSize = True
+        Me.addresslabel.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.addresslabel.Location = New System.Drawing.Point(12, 100)
+        Me.addresslabel.Name = "addresslabel"
+        Me.addresslabel.Size = New System.Drawing.Size(66, 21)
+        Me.addresslabel.TabIndex = 10
+        Me.addresslabel.Text = "Address"
+        '
+        'agelabel
+        '
+        Me.agelabel.AutoSize = True
+        Me.agelabel.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.agelabel.Location = New System.Drawing.Point(807, 100)
+        Me.agelabel.Name = "agelabel"
+        Me.agelabel.Size = New System.Drawing.Size(37, 21)
+        Me.agelabel.TabIndex = 11
+        Me.agelabel.Text = "Age"
+        '
+        'cnlabel
+        '
+        Me.cnlabel.AutoSize = True
+        Me.cnlabel.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.cnlabel.Location = New System.Drawing.Point(12, 156)
+        Me.cnlabel.Name = "cnlabel"
+        Me.cnlabel.Size = New System.Drawing.Size(125, 21)
+        Me.cnlabel.TabIndex = 12
+        Me.cnlabel.Text = "Contact Number"
+        '
+        'emaillabel
+        '
+        Me.emaillabel.AutoSize = True
+        Me.emaillabel.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.emaillabel.Location = New System.Drawing.Point(297, 156)
+        Me.emaillabel.Name = "emaillabel"
+        Me.emaillabel.Size = New System.Drawing.Size(114, 21)
+        Me.emaillabel.TabIndex = 13
+        Me.emaillabel.Text = "E-mail Address"
+        '
+        'question1
+        '
+        Me.question1.AutoSize = True
+        Me.question1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.question1.Location = New System.Drawing.Point(12, 190)
+        Me.question1.Name = "question1"
+        Me.question1.Size = New System.Drawing.Size(419, 21)
+        Me.question1.TabIndex = 14
+        Me.question1.Text = "Have you tested positive for COVID-19 in the past 14 days?"
+        '
+        'question2
+        '
+        Me.question2.AutoSize = True
+        Me.question2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.question2.Location = New System.Drawing.Point(12, 230)
+        Me.question2.Name = "question2"
+        Me.question2.Size = New System.Drawing.Size(700, 21)
+        Me.question2.TabIndex = 15
+        Me.question2.Text = "Is there an individual/s in your household that has tested positive for COVID-19 " &
+    "in the past 14 days?"
+        '
+        'question3
+        '
+        Me.question3.AutoSize = True
+        Me.question3.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.question3.Location = New System.Drawing.Point(12, 270)
+        Me.question3.Name = "question3"
+        Me.question3.Size = New System.Drawing.Size(593, 21)
+        Me.question3.TabIndex = 16
+        Me.question3.Text = "Have you been in contact with someone who has tested positive in the past 14 days" &
+    "?"
+        '
+        'question4
+        '
+        Me.question4.AutoSize = True
+        Me.question4.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.question4.Location = New System.Drawing.Point(12, 310)
+        Me.question4.Name = "question4"
+        Me.question4.Size = New System.Drawing.Size(339, 21)
+        Me.question4.TabIndex = 17
+        Me.question4.Text = "Did you travel outside NCR in the past 14 days?"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(12, 350)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(352, 21)
+        Me.Label1.TabIndex = 18
+        Me.Label1.Text = "Did you travel internationally in the past 14 days?"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(924, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.question4)
+        Me.Controls.Add(Me.question3)
+        Me.Controls.Add(Me.question2)
+        Me.Controls.Add(Me.question1)
+        Me.Controls.Add(Me.emaillabel)
+        Me.Controls.Add(Me.cnlabel)
+        Me.Controls.Add(Me.agelabel)
+        Me.Controls.Add(Me.addresslabel)
+        Me.Controls.Add(Me.milabel)
+        Me.Controls.Add(Me.fnlabel)
+        Me.Controls.Add(Me.lnlabel)
+        Me.Controls.Add(Me.emailbox)
+        Me.Controls.Add(Me.agebox)
+        Me.Controls.Add(Me.cnbox)
+        Me.Controls.Add(Me.addressbox)
+        Me.Controls.Add(Me.mibox)
+        Me.Controls.Add(Me.fnbox)
+        Me.Controls.Add(Me.lnbox)
+        Me.Name = "Form1"
+        Me.Text = "Contact Tracing Form"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents lnbox As TextBox
+    Friend WithEvents fnbox As TextBox
+    Friend WithEvents mibox As TextBox
+    Friend WithEvents addressbox As TextBox
+    Friend WithEvents cnbox As TextBox
+    Friend WithEvents agebox As TextBox
+    Friend WithEvents emailbox As TextBox
+    Friend WithEvents lnlabel As Label
+    Friend WithEvents fnlabel As Label
+    Friend WithEvents milabel As Label
+    Friend WithEvents addresslabel As Label
+    Friend WithEvents agelabel As Label
+    Friend WithEvents cnlabel As Label
+    Friend WithEvents emaillabel As Label
+    Friend WithEvents question1 As Label
+    Friend WithEvents question2 As Label
+    Friend WithEvents question3 As Label
+    Friend WithEvents question4 As Label
+    Friend WithEvents Label1 As Label
 End Class
